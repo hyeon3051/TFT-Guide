3475d4bd49fa0e12b52f8ac63811bebf0029d13b
hyeon3051 <hyeon3051@gmail.com>
2021-09-02 11:01:45 +0900

    server/deploy-settings

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,11 @@ const env = process.env;
 
 module.exports = {
   development: {
-    username: env.DATABASE_USERNAME,
-    password: env.DATABASE_PASSWORD,
-    database: env.DATABASE_NAME,
-    host: env.DATABASE_HOST,
+    username: env.RDS_USERNAME,
+    password: env.RDS_PASSWORD,
+    database: env.RDS_DATABASE_NAME,
+    host: env.RDS_HOSTNAME,
+    port: env.RDS_PORT,
     dialect: 'mysql',
   },
   test: {
